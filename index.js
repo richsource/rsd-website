a2430500f7c56613f94fe1ec9ffbb184037fab4c
Ivan Krumov <ikrumov@richsourcedev.com>
2019-09-03 01:40:24 +0300

    cleanup

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express()
 
 app.use(express.static('site'))
 
-setInterval(function() {
-    http.get("http://www.ikrumov.com");
-}, 300000); // every 5 minutes (300000)
-
 var port = process.env.PORT || 1377;
 app.listen(port);
 
