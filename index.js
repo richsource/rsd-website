826b6c771baee5c144f82d1190f153cc048dc87f
Ivan Krumov <ikrumov@richsourcedev.com>
2019-09-09 21:33:21 +0300

    ping the website periodically, so that it does not fall asleep

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express()
 
 app.use(express.static('site'))
 
+//ping the website periodically, so that it does not fall asleep, free heroku apps fall asleep after a few minutes
+setInterval(function() {
+    http.get("http://www.richsourcedev.com/");
+}, 300000); // every 5 minutes (300000)
+
 var port = process.env.PORT || 1377;
 app.listen(port);
 
